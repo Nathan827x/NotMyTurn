7edb38104a7e3b22dce8388989fd6167a93e8d5a
Nathan Wang <nathan.wang2@gmail.com>
2020-07-22 17:42:17 -0700

    Got the users auth logic working. When there is a user logged in the logout option is shown. When a user is logged out it disappears. Still need to do a lot of cleaning up

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,32 +1,73 @@
 import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Firebase from './firebase';
+import firebase, { authPage, authConfig } from './firebase';
 
 import TodoList from './components/todos-list.component.js';
 import EditTodo from './components/edit-todo.component';
 import CreateTodo from './components/create-todo.component';
-import { NavBar } from './components/navbar.js';
+import NavBar from './components/navbar';
+import SignupLogin from './components/signup-login';
 
 class App extends Component {
     constructor(props) {
         super(props);
 
-        // This will intilize Firebase
-        const fb = Firebase;
-
         this.state = {
-            developers: []
+            email: '',
+            uid: ''
         }
+
+        this.test = this.test.bind(this);
+        this.updateInfo = this.updateInfo.bind(this);
+    }
+
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged(function (user) {
+            if (user) {
+                // User is signed in.
+                const displayName = user.displayName;
+                const email = user.email;
+                // const emailVerified = user.emailVerified;
+                const uid = user.uid;
+                const providerData = user.providerData;
+
+                this.updateInfo(email, uid);
+            } else {
+                console.debug("No User Logged In")
+                // User is signed out.
+                // document.getElementById('sign-in-status').textContent = 'Signed out';
+                // document.getElementById('sign-in').textContent = 'Sign in';
+                // document.getElementById('account-details').textContent = 'null';
+            }
+        }.bind(this), function (error) {
+            console.log(error);
+        }).bind(this);
+    }
+
+    updateInfo(email, uid) {
+        this.setState({
+            email,
+            uid
+        })
+    }
+
+    test() {
+        console.debug("In the test method");
+
+        authPage.start('#firebaseui-auth-container', authConfig);
     }
     render() {
         return (
             <Router>
-                <NavBar />
+                <NavBar loggedIn={this.state.email} />
                 <div className="container">
                     <Switch>
                         <Route path="/" exact>
-                            <TodoList />
+                            <TodoList userInfo={this.state} />
+                        </Route>
+                        <Route path="/auth" exact>
+                            <SignupLogin />
                         </Route>
                         <Route path="/edit/:id">
                             <EditTodo />
@@ -36,6 +77,7 @@ class App extends Component {
                         </Route>
                     </Switch>
                 </div>
+                <link type="text/css" rel="stylesheet" href="https://www.gstatic.com/firebasejs/ui/4.5.1/firebase-ui-auth.css" />
             </Router>
         );
     }
--- fe/src/firebase.js
+++ fe/src/firebase.js
@@ -1,8 +1,20 @@
 import firebase from "firebase/app";
+import * as firebaseui from 'firebaseui'
 import "firebase/auth";
 import { firebaseConfig } from "./keys";
 
-
 firebase.initializeApp(firebaseConfig);
+
+export const authPage = new firebaseui.auth.AuthUI(firebase.auth());
+export const authConfig = {
+    signInSuccessUrl: '/',
+    signInFlow: "popup",
+    signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+
+    // Other config options...
+}
 
 export default firebase;
